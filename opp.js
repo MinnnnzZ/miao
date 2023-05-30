99ea5bb03ac1bada25e81fb93960293a516f0efe
chen <596427791@qq.com>
2023-05-30 22:15:46 +0800

    1

---
+++ opp.js
@@ -0,0 +1,29 @@
+class Vector{
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    plus(vector) {
+        var x = this.x + vector.x
+        var y = this.y + vector.y
+        return new Vector
+    }
+    minus(vector) {
+        var x = this.x - vector.x
+        var y = this.y - vector.y
+        return new Vector(x, y)
+    }
+    get length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y)
+    }
+}
+
+
+class Complex{
+    constructor(real, imag) {
+        this.real = real
+        this.imag = imag
+    }
+    
+}
