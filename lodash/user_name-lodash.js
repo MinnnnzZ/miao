50c63ea3108a954f6a5e802d38917000a354457e
chen <596427791@qq.com>
2023-06-05 09:33:52 +0800

    1

---
+++ lodash/user_name-lodash.js
@@ -0,0 +1,23 @@
+var MinnnnzZ = {
+
+
+    compact: function (array, size) {
+        var result = []
+        for (var i of array) {
+            if (i) {
+                result.push(i)
+            }
+        }
+        return result
+    }
+
+    
+    chunk: function (array, size) {
+        var result = []
+        for (var i = 0; i < array.length; i += size){
+            result.push(array.slice(i, i + size))
+        }
+    }
+
+    
+}
