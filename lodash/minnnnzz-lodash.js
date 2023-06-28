c5acd7717fc28615032b4673dbd5fdd8cb2f56c6
chen <596427791@qq.com>
2023-06-28 16:43:16 +0800

    1

--- lodash/minnnnzz-lodash.js
+++ lodash/minnnnzz-lodash.js
@@ -31,7 +31,87 @@ var minnnnzz = {
         return array.slice(n)
     },
 
-    findIndex: function (array) {}
+    findIndex : function(array,predicate,fromIndex = 0){
+        for(;fromIndex<array.length;fromIndex++){
+          if(typeof predicate == 'function'){
+            for(var key in array[fromIndex]){
+              if(predicate(array[fromIndex])){
+                return fromIndex
+              }
+            }
+          }
+          if(typeof predicate == 'object'){
+            if(Array.isArray(predicate)){
+              for(var key in array[fromIndex]){
+                if(key == predicate[0] && array[fromIndex][key] == predicate[1]){
+                  return fromIndex
+                }
+              }
+            }else{
+              var a = true
+              for(var key in array[fromIndex]){
+    
+                if(array[fromIndex][key] !== predicate[key]){
+                  a = false
+                }
+              }
+              if(a == true){
+                return fromIndex
+              }
+            }
+          }
+          if(typeof predicate == 'string'){
+              if(predicate in array[fromIndex] && array[fromIndex][predicate] == true){
+                return fromIndex
+              }
+            }
+    
+        }
+        return -1
+    },
+    
+      findLastIndex :function(array, predicate = this.identity(predicate), fromIndex=array.length-1){
+        var c = 0
+        var j = fromIndex
+        for(;fromIndex >=0;fromIndex--){
+          if(typeof predicate == 'function'){
+            for(var key in array[fromIndex]){
+              if(predicate(array[fromIndex])){
+                return j - c
+              }
+            }
+          }
+          if(typeof predicate == 'object'){
+            if(Array.isArray(predicate)){
+              for(var key in array[fromIndex]){
+                if(key == predicate[0] && array[fromIndex][key] == predicate[1]){
+                  return j - c
+                }
+              }
+            }else{
+              var a = true
+              for(var key in array[fromIndex]){
+    
+                if(array[fromIndex][key] !== predicate[key]){
+                  a = false
+                }
+              }
+              if(a == true){
+                return j - c
+              }
+            }
+          }
+          if(typeof predicate == 'string'){
+              if(predicate in array[fromIndex] && array[fromIndex][predicate] == true){
+                return j - c
+              }
+            }
+            c++
+        }
+        return -1
+      },
+    
+
 
 
 
