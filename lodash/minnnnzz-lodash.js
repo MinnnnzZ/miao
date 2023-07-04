12dfe63a9f20645ba1edea9805a66c466e421d94
chen <596427791@qq.com>
2023-07-04 22:34:53 +0800

    1

--- lodash/minnnnzz-lodash.js
+++ lodash/minnnnzz-lodash.js
@@ -109,7 +109,21 @@ var minnnnzz = {
             c++
         }
         return -1
-      },
+  },
+      
+  flatten: function (array) {
+    var result = []
+    for (var i = 0; i < array.length; i++){
+      if (Array.isArray(array[i])) {
+        for (var j = 0; j < array[i].length; j++){
+          result.push(array[i][j])
+        }
+      } else {
+        result.push(array[i])
+      }
+    }
+    return 
+  }
     
 
 
